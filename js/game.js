a88c6bc890dd96960d748cfe005eb3e7fb7ffdfe
Ettore Merendino <merendino.ettore@gmail.com>
2016-10-25 17:39:54 -0400

    I added if and else functions to create the property and value in the object

--- js/game.js
+++ js/game.js
@@ -1,7 +1,29 @@
 var stringToSplit = 'A cat in a hat is a silly cat indeed.'
 
-var splitString = stringToSplit.split(' ');
-console.log(splitString);
+function wordFrequency(stringOfWords){
+  var frequencies = {
 
-var splitArray = [splitString];
-console.log(splitString);
+  };
+
+  var splitString = stringOfWords.split(' ');
+  console.log(splitString);
+
+splitString.forEach(function count (word){
+
+  if (frequencies [word] === undefined){
+      frequencies [word] =1;
+    } else {
+      frequencies[word]++;
+    }
+
+});
+
+
+  return frequencies;
+}
+
+var result = wordFrequency(stringToSplit);
+console.log(result);
+
+// var splitArray = [splitString];
+// console.log(splitString);
