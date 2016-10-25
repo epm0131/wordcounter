6913b10d4a22290cf65e88ce6578c315c316aac3
Ettore Merendino <merendino.ettore@gmail.com>
2016-10-25 13:53:49 -0400

    I created my string and then turned it into an array

---
+++ js/game.js
@@ -0,0 +1,7 @@
+var stringToSplit = 'A cat in a hat is a silly cat indeed.'
+
+var splitString = stringToSplit.split(' ');
+console.log(splitString);
+
+var splitArray = [splitString];
+console.log(splitString);
